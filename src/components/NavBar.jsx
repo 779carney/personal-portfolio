c9c6a4173c79144bcff5a1e0d44fd899bab4e884
779carney <ellemaecarney123@gmail.com>
2023-09-05 10:20:19 +0100

    set up basic nav-bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,17 @@
 function NavBar(){
     return (
-        <p>nav bar</p>
+        <div class="shadow bg-white">
+        <div class="h-16 mx-auto px-5 flex items-center justify-between">
+            <a class="text-2xl hover:text-cyan-500 transition-colors cursor-pointer">Logo</a>
+            
+            <ul class="flex items-center gap-5">
+              <li><a class="hover:text-cyan-500 transition-colors" href=""></a>Projects</li>
+              <li><a class="hover:text-cyan-500 transition-colors" href="">Contact Me</a></li>
+              <li><a class="hover:text-cyan-500 transition-colors" href="">Linked in</a></li>
+              <li><a class="hover:text-cyan-500 transition-colors" href="">GitHub</a></li>
+            </ul>
+        </div>
+      </div>
     )
 }
 
