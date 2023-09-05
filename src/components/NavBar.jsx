30cab09279f61ca0a82b4f0f805b10aa15afd78c
779carney <ellemaecarney123@gmail.com>
2023-09-05 15:36:34 +0100

    added an about me section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,14 @@
 function NavBar(){
     return (
-        <div class="shadow bg-white">
-        <div class="h-16 mx-auto px-5 flex items-center justify-between">
-            <a class="text-2xl hover:text-cyan-500 transition-colors cursor-pointer">Logo</a>
+        <div className="shadow bg-white">
+        <div className="h-16 mx-auto px-5 flex items-center justify-between">
+            <a className="text-2xl hover:text-cyan-500 transition-colors cursor-pointer">Logo</a>
             
-            <ul class="flex items-center gap-5">
-              <li><a class="hover:text-cyan-500 transition-colors" href=""></a>Projects</li>
-              <li><a class="hover:text-cyan-500 transition-colors" href="">Contact Me</a></li>
-              <li><a class="hover:text-cyan-500 transition-colors" href="">Linked in</a></li>
-              <li><a class="hover:text-cyan-500 transition-colors" href="">GitHub</a></li>
+            <ul className="flex items-center gap-5">
+              <li><a className="hover:text-cyan-500 transition-colors" href=""></a>Projects</li>
+              <li><a className="hover:text-cyan-500 transition-colors" href="">Contact Me</a></li>
+              <li><a className="hover:text-cyan-500 transition-colors" href="">Linked in</a></li>
+              <li><a className="hover:text-cyan-500 transition-colors" href="">GitHub</a></li>
             </ul>
         </div>
       </div>
