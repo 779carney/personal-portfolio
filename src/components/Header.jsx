3fd8b25b3030516fcb0d47cbb6217ae413351a98
779carney <ellemaecarney123@gmail.com>
2023-09-07 16:44:19 +0100

    i have got the project cards to show on the screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@
 
 function Header(){
    return(
-    <h2 className="mt-0 text-xl font-bold tracking-tight leading-none md:text-4xl xl:text-5xl dark:text-white text-center">Projects</h2>
+    <h2 className="mt-0 mb-1 text-xl font-bold tracking-tight leading-none md:text-4xl xl:text-5xl dark:text-white text-center  ">Projects</h2>
    )}
 export default Header;
 
--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,7 +4,7 @@ import Projects from "./Projects"
 
 function Tech(){
     return (<>
-    <section className="bg-yellow-600 max-w-screen-2xl px-4 py-8 mx-auto ">
+    <section className=" max-w-screen-2xl px-4 py-8 mx-auto min-h-fit grid">
         
 
     <Header/>
