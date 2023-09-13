57fb6923f6186fe9a1996687e583a16c52c9d98a
779carney <ellemaecarney123@gmail.com>
2023-09-13 12:32:15 +0100

    made an alert for if not filled fields in correctly

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -22,7 +22,7 @@ function handleSubmit(event){
   event.preventDefault()
 if(message.trim()=== "" || formName.trim()=== ""){
   setValidMessage(false)
-alert('Please fill in the input fields')
+
 } else {
 
 setIsSuccessfull(true);
@@ -36,7 +36,7 @@ setFormName('')
 }
 
 }
-if(isSuccessfull){
+
   return (
 
     <div id="contact-me" className="flex mt-10 items-center justify-start bg-white ">
@@ -67,48 +67,20 @@ if(isSuccessfull){
           </div>
           <button type="submit" className="mt-5 rounded-md bg-black px-10 py-2 text-white" >Send Message</button>
         </form>
-        <div>
-          <p className="mt-10 mb-10 peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm">Thankyou for your message, I will contact you shortly</p>
-        </div>
+        { isSuccessfull ?  <div>
+          <p className="mt-10 mb-10 peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm">Thank you for your message, I will contact you shortly</p>
+        </div> : null}
+        { !validMessage ?  <div>
+          <p className="mt-10 mb-10 peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm">Please fill in the input fields correctly</p>
+        </div> : null}
       </div>
     </div>
         )
 
-}
-    return (
 
-<div id="contact-me" className="flex mt-10 items-center justify-start bg-white ">
-  <div className="mx-auto w-full max-w-lg mt-16">
-    <h1 className="text-4xl font-medium ">Contact me</h1>
-   
-
-    <form onSubmit={handleSubmit} className="mt-10">
-      <div className="grid gap-6 sm:grid-cols-2">
-        <div className="relative z-0">
-          <input type="text" name="name" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " required value={formName} onChange={(event) => {
-                setFormName(event.target.value)
-            }} /> 
-          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Your name</label>
-        </div>
-        <div className="relative z-0">
-          <input type="email" name="email" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " required value={email} onChange={(event) => {
-                setEmail(event.target.value)
-            }}/>
-          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Your email</label>
-        </div>
-        <div className="relative z-0 col-span-2">
-          <textarea name="message" rows="5" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " required  value={message} onChange={(event) => {
-                setMessage(event.target.value)
-            }}></textarea>
-          <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Your message</label>
-        </div>
-      </div>
-      <button type="submit" className="mt-5 rounded-md bg-black px-10 py-2 text-white hover:text-cyan-500 transition-colors" >Send Message</button>
-    </form>
-  </div>
-</div>
-    )
 }
+    
+
 
 export default ContactMe
 
